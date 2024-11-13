30757fb0bb0eb80e3e9d7355f8557478535bdb74
Spacs21 <behaksr123@gmail.com>
2024-11-13 07:05:54 +0500

    added offset

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -3,9 +3,10 @@ export const initialState = {
     theme: false
 }
 export const reducer = (state, action) => {
+    let offest = 1
     switch(action.type){
         case "INC":
-            return {...state, count: state.count + 1}
+            return {...state, count: state.count + offest}
         case "DEC":
             return {...state, count: state.count >= 1 ? state.count - 1 : 0}
         case "CLR":
